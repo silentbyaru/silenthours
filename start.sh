780ee1197a808b79039e5e61c1c69259e731017e
silentbyaru <arunavamanna623@gmail.com>
2025-06-08 19:34:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/silentbyaru/silenthours
+  git clone https://github.com/silentbyaru/silenthours/tree/TELEGRAM_FILES
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /silenthours
